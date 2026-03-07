69cbf9569dd7d15dce69678c3e2d52654777e714
Mert <223351406+thisismert@users.noreply.github.com>
2026-03-07 13:55:48 +0300

    getting ready for publish

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     format: ['esm', 'cjs'],
     dts: true,
     clean: true,
-    sourcemap: true,
+    sourcemap: false,
     target: 'node18',
     deps: {
         neverBundle: ['vite'],
